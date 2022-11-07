e2d4f2fb066ec29c8d4093c8074a9ad807f8837b
Fernando Gutierrez <110203103+fernandogutierrezz@users.noreply.github.com>
2022-11-07 16:33:33 -0300

    modulo1

---
+++ modulo-1/aula1-sintaxe/ex1.js
@@ -0,0 +1,33 @@
+//statements (instrução) terminam com ponto e virgula
+var x=15;
+var y=12;
+
+//valores: literias e variáveis
+//literais
+10;
+10.5;
+"Olá mundo";
+'Olá mundo';
+
+//variaveis
+var x;
+
+//operadores 
+x = 10; //O operador = agrega valor a uma variavel
+10 + 5; //Soma
+20 - 20; //Subtração
+40 * 5; //Multiplicação
+20 / 10; //Divisão
+
+//expressões
+y + 5;
+20 * 15;
+x * y;
+
+//identificadores
+var sintaxeBasica; //camel case
+//formas permitidas para iniciar a variavel 
+var $1;
+var _1;
+var ola;
+var olaMundo; //mais de uma palavra, iniciar com a letra maiuscula em todas a partir da segunda palavra! (camel case)
---
+++ modulo-1/aula4/ex4.js
@@ -0,0 +1,5 @@
+function soma(n1,n2){
+    var res;
+    res = n1*n2;
+    document.write(res);
+}
